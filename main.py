1a6d575a7029da284e5bc2900d90b9ed7987cd1f
Kuba <j.pikula97@gmail.com>
2023-08-02 13:42:42 +0200

    Layout created

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import tkinter
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = tkinter.Tk()
+window.title('Flash Card App')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = tkinter.Canvas(width=800, height=526)
+card_front_img = tkinter.PhotoImage(file='images/card_front.png')
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'))
+canvas.create_text(400, 263, text='word', font=('Ariel', 60, 'bold'))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
+
+cross_image = tkinter.PhotoImage(file='images/wrong.png')
+unknown_button = tkinter.Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(column=0, row=1)
+
+check_image = tkinter.PhotoImage(file='images/right.png')
+known_button = tkinter.Button(image=check_image, highlightthickness=0)
+known_button.grid(column=1, row=1)
+
+window.mainloop()
